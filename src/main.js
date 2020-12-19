2051f440d887725b45bfb0804003cc10373e38f0
On <5586237+wm1045266943@user.noreply.gitee.com>
2020-12-19 16:21:42 +0800

    '完成了所有功能'

--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ import store from "./store";
 import "./plugins/element.js";
 import TreeTable from "vue-table-with-tree-grid";
 import VueQuillEditor from "vue-quill-editor";
-<<<<<<< HEAD
 
 
-=======
->>>>>>> report
 //导入全局样式表
 import "./assets/css/base.css";
 //导入element-ui样式表
